e9b2eebcd1da3249d3768236eb9d0134e2307428
Rocxs <131322814+Rocxs@users.noreply.github.com>
2023-04-19 19:52:32 -0600

    Create app.R

---
+++ app.R
@@ -0,0 +1,11 @@
+library(shiny)
+#runExample("01_hello")
+
+#importando Data frame 
+
+#getwd()
+setwd("C:/Users/rocha/OneDrive/Documentos/Proyecto_BioinfoAplicada/Data sets/Data sets/Sleep Data/archive (5)")
+
+SetDeDatos<-read.csv("dataset_2191_sleep.csv")
+SetDeDatos
+
